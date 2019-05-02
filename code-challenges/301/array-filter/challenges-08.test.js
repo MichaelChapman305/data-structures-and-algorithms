2a67f605d340ddf2df9c4d5a6cffb1838a88e324
Michael A. Chapman <chapmanm305@gmail.com>
2019-05-01 18:13:17 -0700

    Find solution for challenge 3

--- code-challenges/301/array-filter/challenges-08.test.js
+++ code-challenges/301/array-filter/challenges-08.test.js
@@ -37,7 +37,7 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
-  // Solution code here...
+  return arr.filter(num => forbiddenValues.indexOf(num) === -1);
 };
 
 /* ------------------------------------------------------------------------------------------------
