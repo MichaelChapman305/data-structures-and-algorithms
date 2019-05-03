6bb3cf1ad81b9d8e5d25641e8a094613dc029a2a
Michael A. Chapman <chapmanm305@gmail.com>
2019-05-03 16:45:29 -0700

    Find solution for challenge 6

--- code-challenges/301/array-reduce/challenges-09.test.js
+++ code-challenges/301/array-reduce/challenges-09.test.js
@@ -179,7 +179,13 @@ const isPrime = (value) => {
 };
 
 const countPrimeNumbers = (arr) => {
-  // Solution code here...
+  return arr.reduce((acc, cur) => {
+    if (isPrime(cur)) {
+      acc += 1;
+    }
+
+    return acc;
+  }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
