80573c960e600721db7ad0d9254ad52932b49735
Michael A. Chapman <chapmanm305@gmail.com>
2019-05-03 16:11:06 -0700

    Find solution for challenge 2

--- code-challenges/301/array-reduce/challenges-09.test.js
+++ code-challenges/301/array-reduce/challenges-09.test.js
@@ -69,7 +69,10 @@ let starWarsData = [{
 }];
 
 const returnNames = (arr) => {
-  // Solution code here...
+  return arr.reduce((acc, cur) => {
+    acc.push(cur.name);
+    return acc;
+  }, []);
 };
 
 /* ------------------------------------------------------------------------------------------------
