096e9f23bc5e31fb8138cf4c82d1d557b0056538
Michael A. Chapman <chapmanm305@gmail.com>
2019-05-08 17:25:17 -0700

    Add solution for challenge 8

--- code-challenges/301/string-manipulation/challenges-13.test.js
+++ code-challenges/301/string-manipulation/challenges-13.test.js
@@ -108,7 +108,14 @@ For example, [['Brook Testing', 'Actual Person'], ['Human Person', 'Brook again'
 ------------------------------------------------------------------------------------------------ */
 
 const unenrollBrook = (arr) => {
-  // Solution code here...
+  return arr.map(item => { 
+    return item.reduce((acc, name) => {
+      if (!name.includes('Brook')) {
+        acc.push(name);
+      }
+      return acc;
+    }, [])
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
