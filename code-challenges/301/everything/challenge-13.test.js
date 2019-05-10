b48bf644a2347bd3fa30def897192e955a64d6ec
Michael A. Chapman <chapmanm305@gmail.com>
2019-05-09 17:14:18 -0700

    Add solution for challenge 1

--- code-challenges/301/everything/challenge-13.test.js
+++ code-challenges/301/everything/challenge-13.test.js
@@ -9,7 +9,9 @@ For example, ['apple', 'banana', 'MacGyver'] returns ['Apple', 'Banana', 'MacGyv
 ------------------------------------------------------------------------------------------------ */
 
 const toTitleCase = (arr) => {
-  // Solution code here...
+  return arr.map(item => {
+    return item.charAt(0).toUpperCase() + item.slice(1);
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
