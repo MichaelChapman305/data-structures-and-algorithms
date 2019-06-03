d0dac59755f2e43824ddfae859cbf080f314b73f
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-03 11:30:48 -0700

    Better organize unit tests

--- code-challenges/401/challenges/linkedList/__tests__/linked-list.test.js
+++ code-challenges/401/challenges/linkedList/__tests__/linked-list.test.js
@@ -9,27 +9,33 @@ describe('linked-list module', () => {
     expect(list).toBeDefined();
   });
 
-  it('Insert should take a value and return said value', () => {
-    expect(list.insert(3)).toEqual(3);
+  describe('Insert', () => {
+    it('Should take a value and return said value', () => {
+      expect(list.insert(3)).toEqual(3);
+    });
+  
+    it('Should properly insert multiple nodes', () => {
+      list.insert(8);
+      list.insert(22);
+      expect(list.print()).toEqual([22, 8, 3]);
+    });
+
+    it('Should take a value and return said value', () => {
+      // inserted values from above
+      expect(list.head.val).toEqual(22);
+    });
   });
 
-  it('Insert can properly insert multiple nodes', () => {
-    list.insert(8);
-    list.insert(22);
-    expect(list.print()).toEqual([22, 8, 3]);
-  });
+  describe('includes', () => {
+    it('Should take a value and return true if value is found', () => {
+      expect(list.includes(22)).toEqual(true);
+      expect(list.includes(33)).toEqual(false);
+    });
+  })
 
-  it('Includes should take a value and return true if value is found', () => {
-    expect(list.includes(22)).toEqual(true);
-    expect(list.includes(33)).toEqual(false);
-  });
-
-  it('Insert should take a value and return said value', () => {
-    // inserted values from above
-    expect(list.head.val).toEqual(22);
-  });
-
-  it('Print should return an array of values', () => {
-    expect(list.print()).toEqual([22, 8, 3]);
+  describe('print', () => {
+    it('Should return an array of values', () => {
+      expect(list.print()).toEqual([22, 8, 3]);
+    });
   });
 });
