95484b4bb580b3daaf97f46ea21501a13ee12391
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-05 13:11:54 -0700

    Scaffold challenge

---
+++ code-challenges/401/challenges/llMerge/README.md
@@ -0,0 +1,36 @@
+# Linked List
+Create a linked list data structure using class syntax
+
+## Challenge
+Create insert, includes, and print methods for Linked list class
+
+## Approach & Efficiency
+I utilized class syntax to create a node and linkedlist class and implemented insert, includes, and print methods to make a functioning list. 
+
+## API
+###Insert (add front)
+Takes a value and nserts it into the beginning of the array and resets the head to that new node
+
+###Includes
+Takes a value and iterates over the linked list and returns true if value is found and false if not
+
+###Print
+console logs each value in the linked list and returns an array of all the values in the linked list
+
+###append
+Appends a value to the end of the link list
+
+###appendBefore
+Appends a node before the specified node
+
+###appendAfter
+Append a node after the specified node
+
+####kthAfterEnd
+returns value of node given index from end of LinkedList
+
+## Solution
+![Picture of whiteboarding](../../assets/linked_list1.jpg)
+![Picture of whiteboarding](../../assets/linked_list2.jpg)
+![Picture of whiteboarding](../../assets/linked_list3.jpg)
+![Picture of whiteboarding](../../assets/linked_list4.JPG)
---
+++ code-challenges/401/challenges/llMerge/linked-list.js
@@ -0,0 +1,135 @@
+'use strict';
+
+class Node {
+  constructor() {
+    this.val = null;
+    this.next = null;
+  }
+};
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  insert(val) {
+    let node = new Node();
+    node.val = val;
+    node.next = this.head;
+    this.head = node;
+    length++;
+    return this.head.val;
+  }
+
+  includes(val) {
+    let current = this.head;
+
+    while (current) {
+      if (current.val === val) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    return false;
+  }
+
+  insertBefore(value, newValue) {
+    let newNode = new Node();
+    newNode.val = newValue;
+    let current = this.head;
+    
+    if (current.val === value) {
+      newNode.next = this.head;
+      this.head = newNode;
+      length++;
+      return newValue;
+    } else {
+      while(current) {
+        if (current.next.val === value) {
+          newNode.next = current.next;
+          current.next = newNode;
+          length++;
+          return newValue;
+        } else {
+          current = current.next;
+        }
+      }
+    }
+
+    return 'Node could not be found';
+  }
+
+  insertAfter(value, newValue) {
+    let newNode = new Node();
+    newNode.val = newValue;
+    let current = this.head;
+
+    while(current) {
+      if (current.val === value) {
+        newNode.next = current.next;
+        current.next = newNode;
+        length++;
+        return newValue;
+      } else {
+        current = current.next;
+      }
+    }
+
+    return 'Node could not be found';
+  }
+
+  append(val) {
+    let node = new Node();
+    node.val = val;
+
+    if (!this.head) {
+      this.head = node;
+      length++;
+      return val;
+    } 
+
+    let current = this.head;
+
+    while(current) {
+      if (!current.next) {
+        current.next = node;
+        length++;
+        return val;
+      } else {
+        current = current.next;
+      }
+    }
+    return val;
+  }
+
+  kthFromEnd(k) {
+    let index = (this.length - k) - 1;
+    let current = this.head;
+
+    if (k > this.length || k < 0) {
+      return 'Index does not exist';
+    } else if (k === this.length) {
+      return this.head.val
+    }
+
+    for (let i = 0; i <= index; i++) {
+      if (i === index) {
+        return current.val;
+      } else {
+        current = current.next;
+      }
+    }
+  }
+
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.val);
+      current = current.next;
+    }
+  }
+};
+
+module.exports = LinkedList;
