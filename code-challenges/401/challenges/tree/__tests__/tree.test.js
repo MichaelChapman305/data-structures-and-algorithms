eb03d8cf2adad4bc28d96a6f110d64c48607d85f
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-15 09:48:46 -0700

    Implement inPlace method in the binaryTree class and implement inPlace unit tests

--- code-challenges/401/challenges/tree/__tests__/tree.test.js
+++ code-challenges/401/challenges/tree/__tests__/tree.test.js
@@ -2,11 +2,35 @@
 
 let binaryTree = require('../tree.js');
 
+//          25
+//        /    \ 
+//      15      30
+//     /  \    /  \
+//   10    20 28   35
+
 describe('binaryTree', () => {
   let tree;
   beforeEach(() => {
     tree = new binaryTree();
+
+    // create root
+    tree.root = {value: 25, left: null, right: null};
+
+    // root.left
+    tree.root.left = {value: 15, left: null, right: null};
+    // root.left.left
+    tree.root.left.left = {value: 10, left: null, right: null};
+    // root.left.right
+    tree.root.left.right = {value: 20, left: null, right: null};
+
+    // root.right
+    tree.root.right = {value: 30, left: null, right: null};
+    // root.right.right
+    tree.root.right.right = {value: 35, left: null, right: null};
+    // root.right.left
+    tree.root.right.left = {value: 28, left: null, right: null};
   });
+
   describe('constructor', () => {
     it('Should create a new binaryTree', () => {
       expect(tree).toBeDefined();
@@ -14,4 +38,24 @@ describe('binaryTree', () => {
     });
   });
 
+  describe('inOrder method', () => {
+    it('Should return an array of numbers from least to greatest', () => {
+      let inOrder = tree.inOrder();
+
+      expect(inOrder).toBeDefined();
+      expect(inOrder).toEqual([10, 15, 20, 25, 28, 30, 35]);
+    });
+
+    it('Should not modify the tree', () => {
+      tree.inOrder();
+
+      expect(tree.root.value).toEqual(25);
+      expect(tree.root.left.value).toEqual(15);
+      expect(tree.root.left.left.value).toEqual(10);
+      expect(tree.root.left.right.value).toEqual(20);
+      expect(tree.root.right.value).toEqual(30);
+      expect(tree.root.right.right.value).toEqual(35);
+      expect(tree.root.right.left.value).toEqual(28);
+    });
+  });
 });
--- code-challenges/401/challenges/tree/tree.js
+++ code-challenges/401/challenges/tree/tree.js
@@ -13,7 +13,31 @@ class binaryTree {
     this.root = node;
   }
 
-  
+  inOrder() {
+    let results = [];
+
+      // recursive helper function
+      // This is working by utilizing the event loop
+      // when it hits the leaf, the step before runs where it was left off, on line 25++
+      // timestamp 4:41
+      let _walk = node => {
+        // L Ro R
+
+        // Left
+        if (node.left) _walk(node.left);
+
+        // Ro
+        // Do something
+        results.push(node.value)
+        
+        // Right
+        if (node.right) _walk(node.right);
+      };
+
+      _walk(this.root);
+
+      return results;
+  }
 }
 
 module.exports = binaryTree;
