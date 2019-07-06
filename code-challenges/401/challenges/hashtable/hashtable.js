0e68fcf0c2b04db4bd4e5f2b824329a564c01e2c
Michael A. Chapman <chapmanm305@gmail.com>
2019-07-06 15:32:13 -0700

    Scaffold hashtable creating the hashtable class as well as a LinkedList instantiation

---
+++ code-challenges/401/challenges/hashtable/hashtable.js
@@ -0,0 +1,68 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(value) {
+    if(!value) throw new Error('Invalid value');
+
+    const node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let current = this.head;
+
+    while(current.next) {
+      current = current.next;
+    }
+
+    current.next = node;
+  }
+
+  getWithKey(key) {
+    if (!this.head) return;
+
+    let current = this.head;
+
+    while (current) {
+      if (current.value[0] === key) return current.value[1]; 
+      current = current.next;
+    }
+
+    return;
+  }
+
+  values() {
+    let values = [];
+    let current = this.head;
+
+    while(current) {
+      values.push(current.value);
+      current = current.next;
+    }
+
+    return values;
+  }
+}
+
+class HashTable {
+  constructor(size) {
+    this.size = size || 5,
+    this.buckets = new Array(this.size);
+  }
+
+}
+
+module.exports = {HashTable, LinkedList};
