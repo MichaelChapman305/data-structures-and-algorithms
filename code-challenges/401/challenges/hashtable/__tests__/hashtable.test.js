648976a6df58bc97436edd50174752e6d59a6bde
Michael A. Chapman <chapmanm305@gmail.com>
2019-07-06 16:12:35 -0700

    Write unit tests for hashtable

---
+++ code-challenges/401/challenges/hashtable/__tests__/hashtable.test.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const { Hashtable, LinkedList } = require('../hashTable.js');
+
+describe('Hash Table', () => {
+  test('Should construct a Hash Table', () => {
+    // arrange
+    let expectedSize = 5;
+
+    // act
+    let hashtable = new hashtable(expectedSize);
+
+    // assert
+    expect(hashtable).toBeInstanceOf(hashtable);
+    expect(hashtable.size).toBeDefined();
+    expect(hashtable.size).toBe(expectedSize)
+    expect(hashtable.buckets.length).toBe(expectedSize);
+  });
+
+  describe('hash(key)', () => {
+    test('Should successfully hash a key to an in range value', () => {
+      // arrange
+      let expectedHash = 3;
+      let key = 'cat';
+      let hashtable = new hashtable(5);
+
+      // act
+      let result = hashtable.hash(key);
+
+      // assert
+      expect(result).toBe(expectedHash);
+    });
+
+    test('Should hash foo to 1 with 5 buckets', () => {
+      // arrange
+      let expectedHash = 1;
+      let key = 'foo';
+      let hashtable = new hashtable(5);
+
+      // act
+      let result = hashtable.hash(key);
+
+      // assert
+      expect(result).toBe(expectedHash);
+    });
+  });
+
+  describe('add(key, value)', () => {
+    test('Should add the key value pair to the hashtable', () => {
+      // arrange
+      let key = 'foo';
+      let value = 'bar';
+      let hashtable = new hashtable(5);
+
+      //act
+      hashtable.add(key, value);
+      let result = hashtable.hash(key);
+
+      //assert
+      expect(hashtable.buckets[result].head.value[1]).toBe('bar');
+    });
+
+    test('Should be able to handle a collision', () => {
+      // arrange
+      let hashtable = new hashtable(5);
+
+      // act
+      hashtable.add('key', 'value');
+      hashtable.add('yek', 'anotherValue');
+
+      let result = hashtable.hash('key');
+
+      // assert
+      expect(hashtable.buckets[result].head.value[1]).toBe('value');
+      expect(hashtable.buckets[result].head.next.value[1]).toBe('anotherValue');
+    });
+  });
+
+  describe('get(key)', () => {
+    test('Given a key, should return the value', () => {
+      // arrange
+      let hashtable = new hashtable(5);
+
+      // act
+      hashtable.add('foo', 'bar');
+      hashtable.add('key', 'value');
+
+      // assert
+      expect(hashtable.get('foo')).toBe('bar');
+    });
+
+    test('Should return null if key does not exist', () => {
+      // arrange
+      let hashtable = new hashtable(5);
+
+      // act
+      hashtable.add('key', 'value');
+
+      // assert
+      expect(hashtable.get('foo')).toBe('bar');
+    });
+
+    test('Should return the value from a bucket within a hashtable that has a collision', () => {
+      // arrange
+      let hashtable = new hashtable(5);
+
+      // act
+      hashtable.add('key', 'value');
+      hashtable.add('key', 'anotherValue');
+
+      let result = hashtable.hash('key');
+
+      // assert
+      console.log(hashtable.get('key'));
+      // expect(hashtable.get('key')).toBe('value');
+      // expect(hashtable.buckets[result].head.next.value[1]).toBe('anotherValue');
+    });
+
+    test('Should return the value from a bucket within a hashtable that has a collision', () => {
+      // arrange
+      // let hashtable = new hashTable(5);
+      let hashtable = new HashTable(5);
+
+      // act
+      hashtable.add('key', 'value');
+      hashtable.add('yek', 'anotherValue');
+
+      let result = hashtable.hash('key');
+      let result2 = hashtable.hash('yek');
+
+      // assert
+      expect(hashtable.get('key')).toBe('value');
+      expect(hashtable.get('yek')).toBe('anotherValue');
+    });
+  });
+});
