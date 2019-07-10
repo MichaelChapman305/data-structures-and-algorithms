e0d50db532e389af3da2a580b3e6c6d717b7bf2e
Michael A. Chapman <chapmanm305@gmail.com>
2019-07-10 14:26:04 -0700

    Scaffold challenge and implement unit tests

---
+++ code-challenges/401/challenges/left_join/__tests__/left-join.test.js
@@ -0,0 +1,62 @@
+'use strict';
+
+let HashTable = require('../../hashtable/hashtable');
+let leftJoin = require('../left-join');
+
+describe('left-join', () => {
+  let table1;
+  let table2;
+
+  beforeEach(() => {
+    table1 = new HashTable(5);
+    table1.add('fond', 'enamored');
+    table1.add('wrath', 'angered');
+    table1.add('diligent', 'employed');
+    table1.add('outfit', 'garb');
+    table1.add('guide', 'usher');
+
+    table2 = new HashTable(5);
+    table2.add('fond', 'averse');
+    table2.add('wrath', 'delight');
+    table2.add('diligent', 'idle');
+    table2.add('guide', 'follow');
+    table2.add('flow', 'jam');
+  });
+
+  test('Should return an array of arrays of the joined tables', () => {
+    let result = [ 
+      [ 'wrath', 'angered', 'delight' ],
+      [ 'fond', 'enamored', 'averse' ],
+      [ 'diligent', 'employed', 'idle' ],
+      [ 'outfit', 'garb', null ],
+      [ 'guide', 'usher', 'follow' ] 
+    ]
+
+    expect(leftJoin(table1, table2)).toEqual(result)
+  });
+
+  test('Should only take hashtables', () => {
+    let table3 = 'hello';
+    let table4 = 'world';
+
+    expect(leftJoin(table3, table4)).toBe('Not valid table')
+  });
+
+  test('Should send an empty array if no matches found', () => {
+    let table3 = new HashTable(5);
+    table1.add('fond', 'enamored');
+    table1.add('wrath', 'angered');
+    table1.add('diligent', 'employed');
+    table1.add('outfit', 'garb');
+    table1.add('guide', 'usher');
+
+    let table4 = new HashTable(5);
+    table2.add('cat', 'averse');
+    table2.add('dog', 'delight');
+    table2.add('fish', 'idle');
+    table2.add('elephant', 'follow');
+    table2.add('raindeer', 'jam');
+  
+    expect(leftJoin(table3, table4)).toEqual([])
+  });
+});
